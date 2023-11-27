d2b1eb53862887b84b04d3352fc93ff961ab876d
RikThePixel <rikdenbreejen@tutanota.com>
2023-11-27 12:17:31 +0100

    Fix the gateway auth

--- apps/gateway/src/middleware/auth.ts
+++ apps/gateway/src/middleware/auth.ts
@@ -3,6 +3,8 @@ import { Request, Response, NextFunction } from 'express';
 import { Request as RestanaRequest, Protocol } from "restana";
 import typeIs from 'type-is';
 
+export AuthOptions;
+
 const auth = (opts?: AuthOptions) => {
   const middleware = makeAuth(opts);
 
@@ -16,5 +18,21 @@ const auth = (opts?: AuthOptions) => {
     middleware(strappedReq, res, next);
   }
 }
+
+export const wrapWithAuth = (
+  routes: Array<Route | WebSocketRoute>,
+  opts?: CircuitBreaker.Options,
+): Array<Route | WebSocketRoute> => {
+  return routes.map((route) => {
+    if (route.proxyType !== 'websocket') {
+      const breakerInstance = auth(opts);
+      const middlewares = route.middlewares ?? [];
+      middlewares.push(breakerInstance);
+      route.middlewares = middlewares;
+    }
+
+    return route;
+  });
+};
 
 export default auth;
